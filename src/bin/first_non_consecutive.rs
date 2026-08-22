f6a50eac5164bea279f7a6c79d76ffecbbdd4d5d
Angélica Martins <marttinsangelica@gmail.com>
2026-08-22 09:03:03 -0300

    feat: start first_non_consecutive challenge

---
+++ src/bin/first_non_consecutive.rs
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/58f8a3a27a5c28d92e000144/train/rust
+
+fn main() {
+  let consecutive  = [1,2,3];
+
+  if consecutive.len() < 2 {
+    panic!("Consecutive array must have, at least, two elements")
+  }
+} 
